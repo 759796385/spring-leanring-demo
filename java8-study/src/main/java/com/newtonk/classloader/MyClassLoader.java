b978f06e34a5eb542e0bd39c05bd7da0fb579827
newtonk <759796385@qq.com>
2018-04-08 21:49:55 +0800

    破除全盘委托机制

--- java8-study/src/main/java/com/newtonk/classloader/MyClassLoader.java
+++ java8-study/src/main/java/com/newtonk/classloader/MyClassLoader.java
@@ -4,7 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.net.URLStreamHandlerFactory;
 
 /**
  * 类名称：
@@ -14,34 +13,33 @@ import java.net.URLStreamHandlerFactory;
  */
 public class MyClassLoader extends URLClassLoader {
 
+    private ClassLoader childClassLoader;
+
     public MyClassLoader(URL[] urls, ClassLoader parent) {
         super(urls, parent);
     }
 
-
-    public MyClassLoader(URL[] urls) {
-        super(urls);
+    public void setChildClassLoader(ClassLoader child){
+        this.childClassLoader = child;
     }
 
-
-    public MyClassLoader(URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
-        super(urls, parent, factory);
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        if(childClassLoader!=null){
+            return childClassLoader.loadClass(name);
+        }
+        return super.loadClass(name);
     }
 
     public static void main(String[] args) {
-        URL deleteDemo = Thread.currentThread().getContextClassLoader().
-                getResource("config/deletedemo2.jar");
-//        URL deleteDemo = Thread.currentThread().getContextClassLoader().
-//                getResource("deletedemo2.jar");
-        //父loader是 appclassloader
-        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{deleteDemo},
-                Thread.currentThread().getContextClassLoader());
+        MyClassLoader child  = parentDependChildWithHierarchical();
         try {
-            Class c= myClassLoader.loadClass("com.DemoImpl");
+            //child 中的包依赖 myClassLoader中的包
+            Class c= child.loadClass("com.dependDemo");
             if(c!= null){
                 try {
                     Object obj = c.newInstance();
-                    Method method = c.getDeclaredMethod("print",null);
+                    Method method = c.getDeclaredMethod("depend",null);
                     method.invoke(obj,null);
                 } catch (InstantiationException | IllegalAccessException
                         | NoSuchMethodException | InvocationTargetException e) {
@@ -51,5 +49,53 @@ public class MyClassLoader extends URLClassLoader {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    /**
+     * 依赖的包在子loader中，如何解决扫描不到的问题!!!
+     */
+    private static MyClassLoader parentDependChildWithHierarchical(){
+        URL depend = Thread.currentThread().getContextClassLoader().
+                getResource("dependDemo.jar");
+        //父loader是 appclassloader
+        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{depend},
+                Thread.currentThread().getContextClassLoader());
+        URL deleteDemo1 = Thread.currentThread().getContextClassLoader().
+                getResource("deletedemo.jar");
+        MyClassLoader child = new MyClassLoader(new URL[]{deleteDemo1},myClassLoader);
+        myClassLoader.setChildClassLoader(child);
+        return child;
+    }
+
+    /**
+     * 当父loader依赖的包在子loader中，全盘委托只会查找父级以及他以上的，搜索不到子loader，报classNotFound
+     */
+    private static MyClassLoader parentDependChild(){
+        URL depend = Thread.currentThread().getContextClassLoader().
+                getResource("dependDemo.jar");
+        //父loader是 appclassloader
+        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{depend},
+                Thread.currentThread().getContextClassLoader());
+        URL deleteDemo1 = Thread.currentThread().getContextClassLoader().
+                getResource("deletedemo.jar");
+        MyClassLoader child = new MyClassLoader(new URL[]{deleteDemo1},myClassLoader);
+        return child;
+    }
+
+    /**
+     * 当子loader依赖的包在父loader中，全盘委托会去父类去找
+     */
+    private static MyClassLoader childDependParent(){
+        URL depend = Thread.currentThread().getContextClassLoader().
+                getResource("dependDemo.jar");
+        URL deleteDemo1 = Thread.currentThread().getContextClassLoader().
+                getResource("deletedemo.jar");
+        //父loader是 appclassloader
+        MyClassLoader myClassLoader = new MyClassLoader(new URL[]{deleteDemo1},
+                Thread.currentThread().getContextClassLoader());
+        MyClassLoader child = new MyClassLoader(new URL[]{depend},myClassLoader);
+        return child;
     }
 }
